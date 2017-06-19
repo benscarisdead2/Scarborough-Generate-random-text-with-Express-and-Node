ea524679f7f4984b8c148fc1467739778367afcd
ben scarborough <benscarborough@scarflix.com>
2017-06-19 14:49:29 -0400

    minifed some code

--- main.js
+++ main.js
@@ -1,20 +1,10 @@
 const express = require('express');
 const app = express();
-const loremIpsum = require('lorem-ipsum')
-  , output = loremIpsum({
-      count: 3,
-      units: 'paragraphs'
-  }); 
-
-app.use(express.static('public'));
+const loremIpsum = require('lorem-ipsum');
 
 app.get('/lorem', function(req, res) {
-    res.send(output);    
-})
-
-app.get('/lorem/', function(req, res) {
-res.send(output);    
-})
+    res.send(loremIpsum({count: 3, units: 'paragraphs', format: 'html'}));    
+});
 
 app.listen(3002, function() {
   console.log("Hey, I'm up and running.");  
