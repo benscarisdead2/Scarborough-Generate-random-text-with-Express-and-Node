b95a61e1a42ffb6502cd67da2a39ec7e17f3fab8
ben scarborough <benscarborough@scarflix.com>
2017-06-19 15:06:06 -0400

    minifed some code, added blastoise

--- main.js
+++ main.js
@@ -42,6 +42,4 @@ app.get('/lorem', function(req, res) {
     res.send(loremIpsum({count: 3, units: 'paragraphs', format: 'html'}));    
 });
 
-app.listen(3002, function() {
-  console.log("Hey, I'm up and running.");  
-})
+app.listen(3002)
